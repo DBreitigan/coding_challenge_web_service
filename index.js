af24ac641af9bed02099ff3fe423122a85614874
Daniel Breitigan <danthemanb3@gmail.com>
2017-09-05 20:33:11 -0600

    A simple express server for coding challeneges

---
+++ index.js
@@ -0,0 +1,24 @@
+//Type node index.js to run
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+//use bodyParser() to let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.post('/', function (req, res) {
+  res.send("localhost:3000\ntest : " + req.body.test + "\nanotha_test : " + req.body.anotha_test);
+});
+
+app.post('/test', function (req, res) {
+  res.send("localhost:3000/test\nJSON test : " + req.body.test);
+});
+
+app.listen(3000, function () {
+  console.log("Listening to port 3000!");
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+A simple web service to use in coding challenges @ DGC.
